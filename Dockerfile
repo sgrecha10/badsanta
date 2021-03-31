234900e707480fcfeb30ef07e69d5f60af2ec612
sgrecha10 <sgrecha10@gmail.com>
2021-03-31 21:26:35 +0300

    Main structure

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+#FROM python:3.8.3-slim
+FROM python:3.8
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR /code
+
+COPY requirements.txt /code/
+
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
+
+COPY . /code/
---
+++ badsanta/routers.py
@@ -0,0 +1,70 @@
+class MySQLRouter:
+    """
+    A router to control all database operations on models in the
+    auth and contenttypes applications.
+    """
+    route_app_labels = {'anna'}
+
+    def db_for_read(self, model, **hints):
+        """
+        Attempts to read auth and contenttypes models go to auth_db.
+        """
+        if model._meta.app_label in self.route_app_labels:
+            return 'mysql'
+        return None
+
+    def db_for_write(self, model, **hints):
+        """
+        Attempts to write auth and contenttypes models go to auth_db.
+        """
+        if model._meta.app_label in self.route_app_labels:
+            return 'mysql'
+        return None
+    #
+    # def allow_relation(self, obj1, obj2, **hints):
+    #     """
+    #     Allow relations if a model in the auth or contenttypes apps is
+    #     involved.
+    #     """
+    #     if (
+    #         obj1._meta.app_label in self.route_app_labels or
+    #         obj2._meta.app_label in self.route_app_labels
+    #     ):
+    #        return True
+    #     return None
+    #
+    # def allow_migrate(self, db, app_label, model_name=None, **hints):
+    #     """
+    #     Make sure the auth and contenttypes apps only appear in the
+    #     'auth_db' database.
+    #     """
+    #     if app_label in self.route_app_labels:
+    #         return db == 'auth_db'
+    #     return None
+
+
+class PostgreSQLRouter:
+    def db_for_read(self, model, **hints):
+        """
+        Reads go to a randomly-chosen replica.
+        """
+        return 'default'
+
+    def db_for_write(self, model, **hints):
+        """
+        Writes always go to primary.
+        """
+        return 'default'
+
+    def allow_relation(self, obj1, obj2, **hints):
+        """
+        Relations between objects are allowed if both objects are
+        in the primary/replica pool.
+        """
+        return True
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        """
+        All non-auth models end up in this pool.
+        """
+        return True
